6fd1468e475ab7433182a4fdc213065d0e4a7f18
livekiron <livebangla2021@gmail.com>
2025-11-28 21:47:28 +0600

    Add files via upload

--- pages/api/auth/start.js
+++ pages/api/auth/start.js
@@ -1,16 +1,15 @@
 export default function handler(req, res) {
   const { state } = req.query;
 
-  const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-
+  const GOOGLE_CLIENT_ID = "211687593638-nhqqkfqj53vaiosgd5dm2f3hvhjt0sc0.apps.googleusercontent.com";
   const REDIRECT_URI = "https://kiron-boq.vercel.app/api/auth/callback";
 
   const url =
     "https://accounts.google.com/o/oauth2/v2/auth" +
     ?client_id=${GOOGLE_CLIENT_ID} +
     &redirect_uri=${encodeURIComponent(REDIRECT_URI)} +
-    &response_type=code +
-    &scope=openid%20email%20profile +
+    "&response_type=code" +
+    "&scope=openid%20email%20profile" +
     &state=${state};
 
   res.redirect(url);
