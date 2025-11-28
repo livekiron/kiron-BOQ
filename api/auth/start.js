93bff8c413f21056cc4e488545f4cb59fc9057ee
livekiron <livebangla2021@gmail.com>
2025-11-28 21:24:16 +0600

    Update start.js

--- api/auth/start.js
+++ api/auth/start.js
@@ -1,20 +1,16 @@
 export default function handler(req, res) {
   const { state } = req.query;
 
-  const GOOGLE_CLIENT_ID =
-    "211687593638-p3h4h5u2hh59svp428k2r9n49803ao4v.apps.googleusercontent.com";
-
+  const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
   const REDIRECT_URI = "https://kiron-boq.vercel.app/api/auth/callback";
 
   const url =
-    "https://accounts.google.com/o/oauth2/v2/auth" +
-    ?client_id=${GOOGLE_CLIENT_ID} +
+    https://accounts.google.com/o/oauth2/v2/auth? +
+    client_id=${GOOGLE_CLIENT_ID} +
     &redirect_uri=${encodeURIComponent(REDIRECT_URI)} +
-    "&response_type=code" +
-    "&scope=openid%20email%20profile" +
+    &response_type=code +
+    &scope=openid%20email%20profile +
     &state=${state};
 
   res.redirect(url);
 }
-
-
