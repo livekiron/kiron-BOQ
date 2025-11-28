9c4a2395939be0217f7ec40793cdc25baa5b5cd7
livekiron <livebangla2021@gmail.com>
2025-11-28 19:48:34 +0600

    Update start.js

--- api/auth/start.js
+++ api/auth/start.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
   const { state } = req.query;
 
   const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-  const REDIRECT_URI = "https://boq-1-click-auto-fill-5-and-n-a-and-six.vercel.app/api/auth/callback";
+  const REDIRECT_URI = "https://kiron-boq.vercel.app/api/auth/callback";
 
   const url =
     "https://accounts.google.com/o/oauth2/v2/auth" +
@@ -13,4 +13,5 @@ export default function handler(req, res) {
     &state=${state};
 
   res.redirect(url);
+
 }
